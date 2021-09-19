2a01466b80527b214f4dd78f025c8d4fb0929313
jaocabelao <jooramuu@gmail.com>
2021-09-19 12:54:28 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# URLRAMOOS V3.0
+
+A simple script to generate a hidden url for social engineering.
+
+
+## Legal disclaimer:
+
+Usage of URLRAMOOS for attacking targets without prior mutual consent is illegal. It's the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program 
+## Installing (Tested on Kali Linux/TERMUX 2020.2):
+
+```
+apt install git
+git clone https://github.com/PerezMascato/URLCADIZ-V.2
+apt install python3
+pip3 install pyshorteners
+cd URLRAMOOS-V3.0
+python3 URLRAMOOS_V3.0.py
+```
+```
+kali Linux
+
+git clone https://github.com/PerezMascato/URLCADIZ-V.2
+sudo pip3 install pyshorteners
+cd URLRAMOOS-V3.0
+python3 URLRAMOOS_V3.0.py
+```
---
+++ URLRAMOOS_V3.0.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+#Created by @jaoramoo
+
+import os
+from time import sleep
+import pyshorteners
+
+
+def banner():
+    print("\033[93m--------------------------------------------")
+    print("                URLRAMOO V3.0                   ")
+    print("                              ")
+    print("   encurtar URL para engenharia social   ")
+    print("--------------------------------------------")
+
+def programm():
+    os.system('clear')
+    banner()
+    print("\nColoque url que vai ficar visivel")
+    original_domain = str(input("\nEx:facebook.com: "))
+
+    os.system('clear') 
+    
+    banner()
+    print("\ncoloque tags Exemplo: video-do-presidente-jair-messias-bolsonaro-falou")
+    postlink = str(input("Postlink: "))
+
+    os.system('clear')
+    
+    banner()
+    url_to_short = str(input("coloque o link que você quer esconder:"))
+    s = pyshorteners.Shortener()
+    shorten=(s.dagd.short(f'{url_to_short}'))
+    withoutprotocol = shorten[8:]
+    
+    os.system('clear')
+    banner()
+    print(f"\033[95m\nSeu link encurtado: https://{original_domain}-{postlink}@{withoutprotocol}")
+
+
+    defanother()
+    
+
+def defanother():
+    another=str(input("\033[93m\nconverter outro link? (S/N): ")) 
+    if another == "s":
+        programm()
+
+    elif another == "n":
+        exit()
+
+    else:
+        print("Retry")
+        sleep(3)
+        os.system('clear')
+        defanother()
+
+programm()
+
+
+        
